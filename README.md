d7d5037b3413bc8ce42b05c99af099ecb8dd50e0
Samantha-Ruth <sam.haberman@gmail.com>
2023-09-13 15:47:30 -0600

    created fetch

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # Movie-Fight
-Comparison across two movies
+Comparison across two movies.
+
+Challenges of this project: 
+
+Fetching data about movies
+    using AXIOS
+    - omdbapi.com api
+        -has a searching operation
+        -Index vs Show result - Index is a limited set of attributes about a movie.  Show will show all attributes from a followup request. 
+Autocomplete widget from scratch
+    - list of rules that describe how our autocomplete works
+Styling and .css!
+    - using Bulma
--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log('Hi there!');
+// Fetching movie data
+// Network request using AXIOS
+const fetchData = async () => {
+    const response = await axios.get("http://www.omdbapi.com/", {
+        params: {
+            apikey: 'APIKEY',
+            s: 'avengers'
+        }
+    });
+    console.log(response.data);
+};
+
+fetchData();
+
+// API key: http://www.omdbapi.com/?i=tt3896198&apikey=487df51d
+// URL: http://www.omdbapi.com/?apikey=[yourkey]&
+// parameters: s (interpreted as movie title we are searching for)
+
+// fetching single movie
+
+// Autocomplete Widget design
