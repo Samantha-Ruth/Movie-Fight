1c1ddf217880c2ec23ff96b67249561d426247ae
Samantha-Ruth <sam.haberman@gmail.com>
2023-09-13 16:33:05 -0600

    created debounce helper

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
 // Fetching movie data
 // Network request using AXIOS
-const fetchData = async () => {
-    const response = await axios.get("http://www.omdbapi.com/", {
-        params: {
-            apikey: 'APIKEY',
-            s: 'avengers'
-        }
-    });
-    console.log(response.data);
+const fetchData = async (searchTerm) => {
+  const response = await axios.get("http://www.omdbapi.com/", {
+    params: {
+      apikey: "",
+      s: searchTerm,
+      // i: 'tt0848228'
+    },
+  });
+  console.log(response.data);
 };
-
-fetchData();
 
 // API key: http://www.omdbapi.com/?i=tt3896198&apikey=487df51d
 // URL: http://www.omdbapi.com/?apikey=[yourkey]&
@@ -19,3 +18,24 @@ fetchData();
 // fetching single movie
 
 // Autocomplete Widget design
+
+const input = document.querySelector("input");
+
+//wrapper function that restricts calls to onInput, but returns a function as well
+// creating a re-usable debounce helper function
+const debounce = (func, delay = 500) => {
+  let timeoutId;
+  return (...args) => {
+    if (timeoutId) {
+      clearTimeout(timeoutId);
+    }
+    timeoutId = setTimeout(() => {
+      func.apply(null, args);
+    }, delay);
+  };
+};
+
+const onInput = debounce((event) => {
+  fetchData(event.target.value);
+});
+input.addEventListener("input", onInput);
