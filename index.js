2e367a4ca5309938ec9cbbbcb76aea687e35a6bd
Samantha-Ruth <sam.haberman@gmail.com>
2023-09-13 21:38:38 -0600

    make API work

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const autoCompleteConfig = {
   async fetchData(searchTerm) {
     const response = await axios.get("http://www.omdbapi.com/", {
       params: {
-        apikey: "",
+        apikey: "487df51d",
         s: searchTerm,
       },
     });
